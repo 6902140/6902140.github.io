21990124c8813f29e944b04c5e26c0495abb84b7
6902140 <3356142450@qq.com>
2023-11-21 23:52:22 +0800

    Site updated: 2023-11-21 23:52:22

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    // autoplay: true,
+    audio: [
+        {
+            name: '溯（钢琴版）',
+            artist: '柳轻颂',
+            url: 'http://music.163.com/song/media/outer/url?id=1433584979.mp3',
+            cover: 'http://p1.music.126.net/qX7knUIlpmbJ34UBzCCS6w==/109951164802366812.jpg?param=130y130',
+        },
+        {
+            name: "夢灯籠",
+            artist: 'RADWIMPS',
+            url: '/images/夢灯籠.mp3',
+            cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
+        },
+        {
+            name: '前前前世 (movie ver.)',
+            artist: 'RADWIMPS',
+            url: '/images/前前前世-_movie-ver._-RADWIMPS.mp3',
+            cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
+        },
+        {
+            name: 'スパークル（火花）——《你的名字。》插曲',
+            artist: 'ゆう十',
+            url: 'http://music.163.com/song/media/outer/url?id=1984045303.mp3',
+            cover: 'http://p2.music.126.net/SiTInUUumsPdSdndv1MCyw==/109951167903888878.jpg?param=130y130',
+        },
+        {
+            name: 'euphoria',
+            artist: ' ',
+            url: 'http://music.163.com/song/media/outer/url?id=1814327255.mp3',
+            cover: '/images/susan1.jpg',
+        },
+        {
+            name: '夜行',
+            artist: 'ヨルシカ',
+            url: 'http://music.163.com/song/media/outer/url?id=1428153831.mp3',
+            cover: 'https://p2.music.126.net/rX3QUba-6_CacELe4bqL2g==/109951164767615222.jpg?param=130y130',
+        },
+        {
+            name: '遥远的她',
+            artist: '張子铭',
+            url: 'http://music.163.com/song/media/outer/url?id=557095893.mp3',
+            cover: 'http://p1.music.126.net/Y_Io_ln1-ujiL9eFdyQvmQ==/109951166291345875.jpg?param=130y130',
+        }
+    ]
+});   
